62c46973c77d61cdb9dc8c3a63ce4e30a537928a
Diego <learsi_to@live.com.mx>
2021-09-29 20:45:30 -0600

    Ahora he modificado la versión

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //Comandos básicos git: -git init: para iniciar a utilizar git en el proyecto.
-//-git add <file>: Para poder pasar los archivos del 'working directory' al 'staging area'.
+//-git add <file>: *Para guardar los cambios en un archivo que ya se encuentra en commit* Para poder pasar los archivos del 'working directory' al 'staging area'. De este modo ya podemos empezar a trabajar con git en nuestro código.
 //-git status: para ver en qué estado están mis archivos. (si están en el 'working directory', en 'staging area' o en 'repository').
-//-git commit: para pasar desde el 'staging area' al 'repository'. Es para crear la primera versión.
+//-git commit: para pasar desde el 'staging area' al 'repository'. Es para crear la primera versión. (o el primer "punto de control")
 //-git push: es para subirlo a un repositorio remoto, o a un "servidor" para que otros desarrolladores con permiso puedan acceder al código para trabajar con él.
 //-git pull: en caso de estar trabajando en un código que está en un repositorio remoto, hace que te traigas los cambios que los demás han hecho al código.
 //-git clone: hace una copia desde el servidor central donde está el código a tu computadora local para empezar a trabajar.
+//-git log: para diferenciar cada versión o punto de control.
+//-git checkout -- <archivo>: para descartar los cambios del archivo seleccionado.
+//-git diff <archivo>: Para observar los cambios que se le han hecho al archivo seleccionado que aún no estpan en 'commit'
