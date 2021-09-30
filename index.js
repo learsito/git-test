e3bb05ae3970ac2682f39b4c1504e7a580272a42
Diego <learsi_to@live.com.mx>
2021-09-29 20:24:27 -0600

    Mi primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Comandos básicos git: -git init: para iniciar a utilizar git en el proyecto.
+//-git add <file>: Para poder pasar los archivos del 'working directory' al 'staging area'.
+//-git status: para ver en qué estado están mis archivos. (si están en el 'working directory', en 'staging area' o en 'repository').
+//-git commit: para pasar desde el 'staging area' al 'repository'. Es para crear la primera versión.
+//-git push: es para subirlo a un repositorio remoto, o a un "servidor" para que otros desarrolladores con permiso puedan acceder al código para trabajar con él.
+//-git pull: en caso de estar trabajando en un código que está en un repositorio remoto, hace que te traigas los cambios que los demás han hecho al código.
+//-git clone: hace una copia desde el servidor central donde está el código a tu computadora local para empezar a trabajar.
